97f5dab69a4f9a28504743191bc1033cf7ecc719
foolofatook1 <jf9260@bard.edu>
2017-05-07 11:40:19 -0400

    loading up a bunch of sprites

--- sketch.js
+++ sketch.js
@@ -13,10 +13,21 @@ var picX;
 var picY;
 var picL;
 var picH;
-//start page
+//background for start page
 var startPage;
-//pretty self explanatory...
+//background for profile page
 var profilePage;
+//images for corgi
+var corgi1, corgi2, corgi3, corgi4;
+//images for ibizan
+//brown
+var iEar1B, iEar2B, iEar3B, iEar4B;
+//grey
+var iEar1G, iEar2G, iEar3G, iEar4G;
+//musstard
+var iEar1M, iEar2M, iEar3M, iEar4M;
+//white
+var iEar1W, iEar2W, iEar3W, iEar4W;
 //status input
 var input, button;
 /////////preload/////////
@@ -24,9 +35,36 @@ function preload() {
     //font
     Font = loadFont('assets/coders_crux.ttf');
     //welcome page
-    startPage = loadAnimation('assets/textures/startPage.png');
+    startPage = loadAnimation('assets/textures/webPage/startPage.png');
     //profile page
-    profilePage = loadAnimation('assets/textures/profilePage.png');
+    profilePage = loadAnimation('assets/textures/webPage/profilePage.png');
+    //dog avatar sprites
+    //corgi
+    corgi1 = loadAnimation('assets/textures/dogSprites/corgi_1.gif');
+    corgi2 = loadAnimation('assets/textures/dogSprites/corgi_2.gif');
+    corgi3 = loadAnimation('assets/textures/dogSprites/corgi_3.gif');
+    corgi4 = loadAnimation('assets/textures/dogSprites/corgi_4.gif');
+    //ibizan
+    //brown
+    iEar1B = loadAnimation('assets/textures/dogSprites/ibizan_ear1_brown.gif');
+    iEar2B = loadAnimation('assets/textures/dogSprites/ibizan_ear2_brown.gif');
+    iEar3B = loadAnimation('assets/textures/dogSprites/ibizan_ear3_brown.gif');
+    iEar4B = loadAnimation('assets/textures/dogSprites/ibizan_ear4_brown.gif');
+    //grey
+    iEar1G = loadAnimation('assets/textures/dogSprites/ibizan_ear1_grey.gif');
+    iEar2G = loadAnimation('assets/textures/dogSprites/ibizan_ear2_grey.gif');
+    iEar3G = loadAnimation('assets/textures/dogSprites/ibizan_ear3_grey.gif');
+    iEar4G = loadAnimation('assets/textures/dogSprites/ibizan_ear4_grey.gif');
+    //mustard
+    iEar1M = loadAnimation('assets/textures/dogSprites/ibizan_ear1_mustard.gif');
+    iEar2M = loadAnimation('assets/textures/dogSprites/ibizan_ear2_mustard.gif');
+    iEar3M = loadAnimation('assets/textures/dogSprites/ibizan_ear3_mustard.gif');
+    iEar4M = loadAnimation('assets/textures/dogSprites/ibizan_ear4_mustard.gif');
+    //white
+    iEar1W = loadAnimation('assets/textures/dogSprites/ibizan_ear1_white.gif');
+    iEar2W = loadAnimation('assets/textures/dogSprites/ibizan_ear2_white.gif');
+    iEar3W = loadAnimation('assets/textures/dogSprites/ibizan_ear3_white.gif');
+    iEar4W = loadAnimation('assets/textures/dogSprites/ibizan_ear4_white.gif');
 }
 
 function setup() {
@@ -54,11 +92,11 @@ function setup() {
 
 function draw() {
     welcomePage();
-    if (makeProfile >= 1) {
+    if (makeProfile >= 1 && makePic == 0) {
         editProfile();
     }
     if (makePic >= 1) {
-        choosePic();
+    choosePic();
     }
 }
 
@@ -103,5 +141,35 @@ function editProfile() {
 }
 
 function choosePic() {
+    //background
     animation(startPage, 400, 400);
+    //corgi
+    animation(corgi1, 250, 100);
+    if (mouseIsPressed && mouseX >= 250 && mouseX <= 345 && mouseY >= 100 && mouseY <= 200){
+        createProfile -= 1;
+    }
+    animation(corgi2, 350, 100);
+    animation(corgi3, 450, 100);
+    animation(corgi4, 400+150, 100);
+    //ibizan
+    //brown
+    animation(iEar1B, 100+150, 200);
+    animation(iEar2B, 200+150, 200);
+    animation(iEar3B, 300+150, 200);
+    animation(iEar4B, 400+150, 200);
+    //grey
+    animation(iEar1G, 100+150, 325);
+    animation(iEar2G, 200+150, 325);
+    animation(iEar3G, 300+150, 325);
+    animation(iEar4G, 400+150, 325);
+    //mustard
+    animation(iEar1M, 100+150, 450);
+    animation(iEar2M, 200+150, 450);
+    animation(iEar3M, 300+150, 450);
+    animation(iEar4M, 400+150, 450);
+    //white
+    animation(iEar1W, 100+150, 575);
+    animation(iEar2W, 200+150, 575);
+    animation(iEar3W, 300+150, 575);
+    animation(iEar4W, 400+150, 575);
 }
