8059f196039fb28d77b0a32db480d91dbdbba2b4
foolofatook1 <jf9260@bard.edu>
2017-05-07 10:02:00 -0400

    adding profile picture chooser page

--- sketch.js
+++ sketch.js
@@ -6,6 +6,8 @@ var rectL;
 var rectH;
 //make profile counter
 var makeProfile;
+//make profile picture
+var makePic;
 //choose profile picture
 var picX;
 var picY;
@@ -15,13 +17,16 @@ var picH;
 var startPage;
 //pretty self explanatory...
 var profilePage;
+//status input
+var input, button;
+/////////preload/////////
 function preload() {
     //font
     Font = loadFont('assets/coders_crux.ttf');
     //welcome page
     startPage = loadAnimation('assets/textures/startPage.png');
     //profile page
-     profilePage = loadAnimation('assets/textures/profilePage.png');
+    profilePage = loadAnimation('assets/textures/profilePage.png');
 }
 
 function setup() {
@@ -37,15 +42,23 @@ function setup() {
     picY = 40;
     picL = 270;
     picH = 295;
+    //status input
+    //input = createInput();
+    //input.position(414, 83);
+    //input.size(396, 256);
     //counter that changes to profile page
     makeProfile = 0;
-
+    //counter thata changes profile picture
+    makePic = 0;
 }
 
 function draw() {
     welcomePage();
     if (makeProfile >= 1) {
         editProfile();
+    }
+    if (makePic >= 1) {
+        choosePic();
     }
 }
 
@@ -65,15 +78,30 @@ function welcomePage() {
 }
 
 function editProfile() {
+    //background
     animation(profilePage, 400, 400);
+    //clickable rectangle for profile picture
     fill(170);
     noStroke();
     rect(picX, picY, picL, picH);
+    //stage change for profile picture
+    if (mouseIsPressed && mouseX >= picX && mouseX <= picX + picL && mouseY >= picY && mouseY <= mouseY + picH) {
+        makePic += 1;
+    }
+    //status input
+    //input = createInput();
+    //input.position(414, 83);
+    //input.size(396, 256);
+    //text
     fill(255, 0, 220);
-    textFont (Font, 24);
+    textFont(Font, 24);
     textAlign(CENTER);
     text('choose image', 100, 325);
     text('write about yourself', 550, 55);
     text('likes', 65, 430);
     text('dislkes', 75, 630);
+}
+
+function choosePic() {
+    animation(startPage, 400, 400);
 }
